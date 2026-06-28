ebc55753d0fd63cf95c8166d97b716514b5ff2f6
Hariprasadkorrapolu <hariprasadkorrapolu03@gmail.com>
2026-06-28 22:25:14 +0530

    Configure Django settings for Render

--- wastewise/settings.py
+++ wastewise/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u8x&*&kyhljkin#sazxz976m+^+o@b4ey#p+*8m19u)#xuzr!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver", "192.168.31.245"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver", "192.168.31.245",".onrender.com"]
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'static',
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
